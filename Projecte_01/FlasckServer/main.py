fd379b9bffe7285d9297b922978c8f03a1bd3b52
andsan5 <andsansan5@alu.edu.gva.es>
2025-02-25 12:19:23 +0100

    d

--- Projecte_01/FlasckServer/main.py
+++ Projecte_01/FlasckServer/main.py
@@ -1,18 +1,26 @@
-from flask import Flask, jsonify, request
 import pandas as pd
 import joblib, os
-from sklearn.preprocessing import LabelEncoder, StandardScaler, OneHotEncoder
 import datetime
+
+from flask import Flask, jsonify, request
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from pymongo import MongoClient
 
 modelo_path = os.path.join(os.path.dirname(__file__),"./model")
 
 scaler = joblib.load(os.path.join(modelo_path,"scaler.joblib"))
 encoder = joblib.load(os.path.join(modelo_path,"encoder_ohe.joblib"))
 model = joblib.load(os.path.join(modelo_path,"model.joblib"))
+X_train = pd.read_csv(os.path.join(modelo_path,"X_train.csv"))
+X_train.drop(['_id'], inplace = True, axis = 1)
+cliente = MongoClient("mongodb://localhost:27020/")
+db = cliente["PIA"]
+coleccion = db["peticions"]
 
 app=Flask(__name__)
 
 def prediure_preu(peticion):
+    peticion['precio_actual'] = 0
     peticion_df = pd.DataFrame([peticion])
     peticion_scaled = scaler.transform(peticion_df.select_dtypes(include=['number']))
     peticion_encoder = encoder.transform(peticion_df.select_dtypes(include=['object']))
@@ -22,27 +30,26 @@ def prediure_preu(peticion):
 
     peticion_final = pd.concat([peticion_df_scaled, peticion_df_encoded], axis=1)
     peticion_final.drop(['precio_actual'], inplace = True, axis = 1)
-
-    X_train = pd.read_csv(os.path.join(modelo_path,"X_train.csv"))
-    X_train.drop(['_id'], inplace = True, axis = 1)
     peticion_final = peticion_final[X_train.columns]
 
     prediccio = model.predict(peticion_final)
 
-    peticion_scaled_with_pred = peticion_scaled.copy()
-    peticion_scaled_with_pred[:, peticion_df_scaled.columns.get_loc('precio_actual')] = prediccio
-    prediccion_escalada_inversa = scaler.inverse_transform(peticion_scaled_with_pred)
+    peticion_scaled_pred = peticion_scaled.copy()
+    indice_precio_actual = peticion_df_scaled.columns.get_loc('precio_actual')
 
-    prediccion_final = prediccion_escalada_inversa[0, peticion_df_scaled.columns.get_loc('precio_actual')]
+    peticion_scaled_pred[:, indice_precio_actual] = prediccio 
+    prediccion_escalada_inversa = scaler.inverse_transform(peticion_scaled_pred)
+
+    prediccion_final = prediccion_escalada_inversa[0, indice_precio_actual] 
 
     return prediccion_final
 
-def indicar_nivel_oferta(p_cliente, p_modelo):
-    # Ver segun el precio del cliente i del modelo si es una buena o mala oferta, despues devlover un numero de nivel de oferta por ejemplo
-    # 1 = Buena oferta
-    # 2 = Precio asecible
-    # 3 = Mala oferta
-    return 1
+def guardar_mongodb(peticion, preu_model, opinion_cliente):
+    peticion['precio_modelo'] = preu_model
+    if opinion_cliente:
+        peticion['opinion_client'] = opinion_cliente
+
+    coleccion.insert_one(peticion)
 
 @app.route("/model_v1", methods=["POST"])
 def enviar():
@@ -51,7 +58,7 @@ def enviar():
         return jsonify({"error": "No se recibieron datos"}), 400
 
     peticion = {
-        "ano":datos.get("ano"),
+        "ano": datos.get("ano"),
         "almacenamiento": datos.get("almacenamiento"),
         "marca": datos.get("marca"),
         "pantalla_in": datos.get("pantalla_in"), 
@@ -65,14 +72,13 @@ def enviar():
         "bateria": datos.get("bateria"),  
         'promedio_valoraciones': datos.get("promedio_valoraciones"),
         'precio_anterior': datos.get("precio_anterior"),
-        'precio_actual': datos.get("precio_actual")
     }
-    preu_model = prediure_preu(peticion)
-    oferta = indicar_nivel_oferta(peticion.get("precio_actual"), preu_model)
+    preu_model = prediure_preu(peticion.copy())
     timestamp = int(datetime.datetime.now().timestamp() * 1000)
-    return jsonify({"Nivel_oferta": oferta, "timestamp": timestamp})
+    guardar_mongodb(peticion, preu_model, datos.get("opinion_cliente"))
+    return jsonify({"precio_modelo": preu_model, "timestamp": timestamp})
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
-    # app.run(host="127.0.0.1", port=8080)
-# Necesite una funció per a poder recibir les dades de el client i de lo que me retorne el model guardar-ho en una base de datos 
+    app.run(host="127.0.0.1", port=8085, debug=True)
+    # app.run(host="127.0.0.1", port=8085)
+# Gestiuonar Errores
